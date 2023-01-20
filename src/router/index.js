d6f728e0327a9ca159c24ecb34c5742fc9e6c155
Fanny Gautier <fannygautier92@gmail.com>
2023-01-20 15:51:11 +0100

    fix deployement issues v3

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import JournalView from '../views/JournalView.vue'
 import ClavierView from'../views/ClavierView.vue'
 import ContactView from '../views/ContactView.vue'
